08ea340c68abec3f8b5c229f0400c3807aa844ea
HYPER <meerhadikhan364@gmail.com>
2023-01-08 16:06:31 +0500

    java class one

--- app/src/main/java/com/example/java_program/MainActivity2.java
+++ app/src/main/java/com/example/java_program/MainActivity2.java
@@ -2,13 +2,35 @@ package com.example.java_program;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button button=findViewById(R.id.btn);
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i=new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
