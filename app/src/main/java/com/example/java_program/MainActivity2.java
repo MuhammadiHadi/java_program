de452950e1371a87c6d1d6d38d6062015a05ed93
HYPER <meerhadikhan364@gmail.com>
2023-01-09 11:03:14 +0500

    first class java

--- app/src/main/java/com/example/java_program/MainActivity2.java
+++ app/src/main/java/com/example/java_program/MainActivity2.java
@@ -1,7 +1,10 @@
 package com.example.java_program;
 
+import static com.example.java_program.R.id.button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -19,10 +22,19 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Button button=findViewById(R.id.btn);
+        Button btn=findViewById(R.id.button);
 
 
 
         button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i=new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(i);
+            }
+        });
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
